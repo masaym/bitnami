e1e1bb5c6db345541cf76a2f91d2de4a36bc4706
mmatsuo <mmatsuo@mym-eng.com>
2016-11-26 01:14:17 +0000

    init

---
+++ apps/wordpress/htdocs/wp-config.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'bitnami_wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'bn_wordpress');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'dd3aa6f8dd');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', 'f7c785e837f1dae8e92713b3946376bacdb4649d5c1becf2445dbc7487cbc834');
+define('SECURE_AUTH_KEY', '38adacfef68c3ad26d63d3576322d769e23e54174961ba4badd06ff448e34489');
+define('LOGGED_IN_KEY', 'c8e05c786d7d970992f3fc93a5606edf13282478dd812ec3534eae5848f68442');
+define('NONCE_KEY', 'b01839c3ed75fb45971338eadc06feabd47a83309f700382c86b1abb2100a707');
+define('AUTH_SALT', '56039e10c32b25eb65cb6398c1f37e4f71eeb96902ae54a6504c732149717fb3');
+define('SECURE_AUTH_SALT', '11e55489ba61ca681e6d0e91d561a8065352dc1e54018ced88eb655ff56966c3');
+define('LOGGED_IN_SALT', '82bab31642ad3df9f5dea1e42995ed33b9e84c06eb98c3c188c6393648e7555e');
+define('NONCE_SALT', '07b7bbc4683b8338cc1e73f3a5a3252e9af381543bcb283fdc0284b27b5c6d8a');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
+
+
+define('FS_METHOD', 'direct');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/?page=apps&name=wordpress&section=how-to-re-enable-the-xml-rpc-pingback-feature
+
+// remove x-pingback HTTP header
+add_filter('wp_headers', function($headers) {
+    unset($headers['X-Pingback']);
+    return $headers;
+});
+// disable pingbacks
+add_filter( 'xmlrpc_methods', function( $methods ) {
+        unset( $methods['pingback.ping'] );
+        return $methods;
+});
+add_filter( 'auto_update_translation', '__return_false' );
---
+++ apps/wordpress/htdocs/wp-content/themes/mandy/readme.txt
@@ -0,0 +1,31 @@
+=== Description ===
+In this version you will get the theme Mandy color variant. Couple of templates added which is not found in the parent appointment theme.
+
+=== Support ===
+For any ideas, support and feedback you can access the theme forum.
+
+
+Mandy WordPress Theme, Copyright 2016 Webriti Theme
+Mandy is distributed under the terms of the GNU GPL
+
+Screenshot Image
+URL: http://pixabay.com/en/office-home-office-creative-apple-581131/
+Source:http://pixabay.com
+License: CC0 Public Domain
+
+== Version ==
+------ 1.0 ------
+1. Released.
+
+------ 1.0.1 ------ 
+1. Solved Theme Review Issue.
+
+------ 1.1 ------
+1. Added Theme URI.
+2. Update Theme Tag.
+
+------ 1.2 ------
+1. Solved Responsive issue.
+
+------ 1.2.1 ------
+1. Added Spanish Locale
